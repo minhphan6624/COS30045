505994c3d69870568f6c6686a2ab7b665e13f310
minhphan6624 <quangminh6624@gmail.com>
2024-04-02 15:33:19 +1100

    Task 5.2 in progress

--- Week05/Task01/index.js
+++ Week05/Task01/index.js
@@ -1,53 +1,89 @@
-        var w = 500;
-        var h = 100;
+var w = 700;
+var h = 200;
 
-        var dataset = [14, 10, 12, 5, 13, 6, 9, 20, 25]
+var dataset = [14, 10, 12, 5, 13, 6, 9, 20, 25, 40];
 
-        var padding = 1;
+var padding = 1;
 
-        var xScale = d3.scaleBand()
-                    .domain(d3.range(dataset.length ))
-                    .rangeRound([0,w])
-                    .paddingInner(0.05);
+var xScale = d3.scaleBand()
+    .domain(d3.range(dataset.length))
+    .rangeRound([0, w])
+    .paddingInner(0.05);
 
-        var yScale = d3.scaleLinear()
-                    .domain(d3.max(dataset))
-                    .rangeRound([0,w])
-                    .paddingInner(0.05);
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, h]);
 
-        var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-        //Drawing the bars
-        svg.selectAll("rect")
-            .data(dataset).enter()
-            .append("rect")
-            .attr("x", (d, i) => {
-                return xScale(i) ;
-            })
-            .attr("y", (d) => {
-                return (h - d * 4); //set base line at the bottom
-            })
-            .attr("width", xScale.bandwidth())
-            .attr("height", (d) => {
-                return d * 4;
-            })
-            .attr("fill", "lightblue");
+//Drawing the bars
+svg.selectAll("rect")
+    .data(dataset).enter()
+    .append("rect")
+    .attr("x", (d, i) => {
+        return xScale(i);
+    })
+    .attr("y", (d) => {
+        return h - yScale(d); //set base line at the bottom
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => {
+        return yScale(d);
+    })
+    .attr("fill", "lightblue");
 
-        //Drawing the labels
-        svg.selectAll("text")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .text((d) => {
-                return d;
-            })
-            .attr("x", (d, i) => {
-                return i * (w / dataset.length) + (w / dataset.length) / 2 - 5; //Position the text to be in the middle
-            })
-            .attr("y", (d) => {
-                return h - (d * 4) + 15; //Text inside the column 
-            })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "11px")
-            .attr("fill", "white");
-        ;
+//Drawing the labels
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text((d) => {
+        return d;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", (d, i) => {
+        return xScale(i) + xScale.bandwidth() / 2; //Position the text to be in the middle
+    })
+    .attr("y", (d) => {
+        return h - yScale(d) + 14; //Text inside the column 
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "white");
+;
+
+d3.select("button").on("click", () => {
+
+    let numValues = dataset.length;
+
+    dataset = [];
+
+    for (var i = 0; i < numValues; i++) {
+        var newNum = Math.floor(Math.random() * 30);
+        dataset.push(newNum);
+    }
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .attr("y", (d) => {
+            return h - yScale(d); //set base line at the bottom
+        })
+        .attr("height", (d) => {
+            return yScale(d);
+        })
+        .attr("fill", "lightblue");
+
+    svg.selectAll("text")
+        .data(dataset)
+        .text((d) => {
+            return d;
+        })
+        .attr("x", (d, i) => {
+            return xScale(i) + xScale.bandwidth() / 2;
+        })
+        .attr("y", (d) => {
+            return h - yScale(d) + 14;
+        });
+});
+
+
---
+++ Week05/Task02/index.js
@@ -0,0 +1,182 @@
+var w = 700;
+var h = 200;
+
+var dataset = [14, 10, 12, 5, 13, 6, 9, 20, 25, 40];
+
+var padding = 1;
+
+var xScale = d3.scaleBand()
+    .domain(d3.range(dataset.length))
+    .rangeRound([0, w])
+    .paddingInner(0.05);
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, h]);
+
+var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+//Drawing the bars
+svg.selectAll("rect")
+    .data(dataset).enter()
+    .append("rect")
+    .attr("x", (d, i) => {
+        return xScale(i);
+    })
+    .attr("y", (d) => {
+        return h - yScale(d); //set base line at the bottom
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => {
+        return yScale(d);
+    })
+    .attr("fill", "lightblue");
+
+//Drawing the labels
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text((d) => {
+        return d;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", (d, i) => {
+        return xScale(i) + xScale.bandwidth() / 2; //Position the text to be in the middle
+    })
+    .attr("y", (d) => {
+        return h - yScale(d) + 14; //Text inside the column 
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "black");
+;
+
+//Transition 1
+d3.select(".update").on("click", () => {
+
+    let numValues = dataset.length;
+
+    dataset = [];
+
+    for (var i = 0; i < numValues; i++) {
+        var newNum = Math.floor(Math.random() * 30);
+        dataset.push(newNum);
+    }
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .transition()
+        .delay((d, i) => {
+            return i / dataset.length * 100;
+        })
+        .duration(1000)
+        .ease(d3.easeBounceOut)
+        .attr("y", (d) => {
+            return h - yScale(d); //set base line at the bottom
+        })
+        .attr("height", (d) => {
+            return yScale(d);
+        })
+        .attr("fill", "lightblue");
+
+    svg.selectAll("text")
+        .data(dataset)
+        .transition()
+        .delay((d, i) => {
+            return i / dataset.length * 100
+        })
+        .duration(1000)
+        .ease(d3.easeBounceOut)
+        .text((d) => {
+            return d;
+        })
+        .attr("x", (d, i) => {
+            return xScale(i) + xScale.bandwidth() / 2;
+        })
+        .attr("y", (d) => {
+            return h - yScale(d) + 14;
+        });
+});
+
+//Transition 1
+d3.select(".trans1").on("click", () => {
+
+    let numValues = dataset.length;
+
+    dataset = [];
+
+    for (var i = 0; i < numValues; i++) {
+        var newNum = Math.floor(Math.random() * 30);
+        dataset.push(newNum);
+    }
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .transition()
+        .duration(2000)
+        .ease(d3.easePolyInOut)
+        .attr("y", (d) => {
+            return h - yScale(d); //set base line at the bottom
+        })
+        .attr("height", (d) => {
+            return yScale(d);
+        })
+        .attr("fill", "yellow");
+
+    svg.selectAll("text")
+        .data(dataset)
+        .transition()
+        .duration(2000)
+        .ease(d3.easePolyInOut)
+        .text((d) => {
+            return d;
+        })
+        .attr("x", (d, i) => {
+            return xScale(i) + xScale.bandwidth() / 2;
+        })
+        .attr("y", (d) => {
+            return h - yScale(d) + 14;
+        });
+});
+
+//Transition 2
+d3.select(".trans2").on("click", () => {
+
+    let numValues = dataset.length;
+
+    dataset = [];
+
+    for (var i = 0; i < numValues; i++) {
+        var newNum = Math.floor(Math.random() * 30);
+        dataset.push(newNum);
+    }
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .transition()
+        .duration(2000)
+        .ease(d3.easeElasticOut)
+        .attr("y", (d) => {
+            return h - yScale(d); //set base line at the bottom
+        })
+        .attr("height", (d) => {
+            return yScale(d);
+        })
+        .attr("fill", "green");
+
+    svg.selectAll("text")
+        .data(dataset)
+        .transition()
+        .duration(2000)
+        .ease(d3.easeElasticOut)
+        .text((d) => {
+            return d;
+        })
+        .attr("x", (d, i) => {
+            return xScale(i) + xScale.bandwidth() / 2;
+        })
+        .attr("y", (d) => {
+            return h - yScale(d) + 14;
+        });
+});
